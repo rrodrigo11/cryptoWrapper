1fd8a73491b24c085cc2c7e432064e2e12e236d8
Rodrigo Rayas <rodrigo@altomobile.io>
2021-12-04 20:13:10 -0600

    Fix on some typos

--- cryptoWrapper/main.cpp
+++ cryptoWrapper/main.cpp
@@ -16,29 +16,35 @@ int main()
     int choice = 1;
     do
     {
-        cout << "\n0************************ -- MENU -- ***************************************\n";
+        cout << "\n************************ -- MENU -- ***************************************\n";
         cout << "1. Generación y Recuperación de Claves hacia o desde 1 archivo\n";
         cout << "2. Cifrado de Archivos\n";
         cout << "3. Descifrado de Archivos\n";
-        cout << "4.Firma de Archivos\n";
-        cout << "5.Verificación de Firma de Archivos\n";
-        cout << "0.Salir\n";
+        cout << "4. Firma de Archivos\n";
+        cout << "5. Verificación de Firma de Archivos\n";
+        cout << "0. Salir\n";
         cout << "************************ -- END MENU -- ***************************************\n";
         cout << "\nEnter 1, 2, 3, 4, 5 or 0 : ";
         cin >> choice;
         switch (choice)
         {
         case 1:
-            cout << "what is the radius of the circle?\n";
+            cout << "\nGeneración y Recuperación de Claves\n";
             break;
         case 2:
-            cout << "what is the length of the rectangle?\n ";
+            cout << "\nCifrado de Archivos\n ";
             break;
         case 3:
-            cout << "What is the length of the triangle's b-ase?\n ";
+            cout << "\nDescifrado de Archivos\n ";
             break;
         case 4:
-            cout << "goodbye\n";
+            cout << "\nFirma de Archivos\n";
+            break;
+        case 5:
+            cout << "\nVerificación de firma de Archivos\n";
+            break;
+        default:
+            cout << "\nPlease enter a correct option.\n";
             break;
         }
     }while(choice!=0);
